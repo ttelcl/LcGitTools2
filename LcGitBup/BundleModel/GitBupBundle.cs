5a35323e81829a721318eeddd32bba3c0aa1717b
ttelcl <9429819+ttelcl@users.noreply.github.com>
2023-07-18 23:36:47 +0300

    initial work on "gitbup run" and its support in LcGitBup

---
+++ LcGitBup/BundleModel/GitBupBundle.cs
@@ -0,0 +1,106 @@
+﻿/*
+ * (c) 2023  ttelcl / ttelcl
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LcGitBup.BundleModel;
+
+/// <summary>
+/// Models a backup unit
+/// </summary>
+public class GitBupBundle
+{
+  /// <summary>
+  /// Create a new GitBupBundle
+  /// </summary>
+  public GitBupBundle(
+    string folder,
+    string prefix,
+    int tier,
+    string id,
+    string? refId)
+  {
+    Folder = folder;
+    Prefix = prefix;
+    Tier = tier;
+    Id = id;
+    RefId = refId;
+    var refId2 = String.IsNullOrEmpty(RefId) ? "-" : RefId;
+    BundleFileName = $"{Prefix}.{Id}.{refId2}.t{Tier}.bundle";
+    MetaFileName = BundleFileName + ".meta.json";
+  }
+
+  public static GitBupBundle? FromBundleName(string fileName)
+  {
+    var shortName = Path.GetFileName(fileName);
+    var segments = shortName.Split('.');
+    if(segments.Length < 5)
+    {
+      return null;
+    }
+    var extension = segments[^1].ToLowerInvariant();
+    if(extension != "bundle")
+    {
+      return null;
+    }
+    var tierText = segments[^2].ToLowerInvariant();
+    if(tierText.Length != 2 || tierText[0] != 't' || tierText[1]<'0' || tierText[1]>'9')
+    {
+      return null;
+    }
+    var tier = tierText[1] - '0';
+    var refId = segments[^3];
+    if(tier == 0 && refId != "-")
+    {
+      return null;
+    }
+    var id = segments[^4];
+    var prefix = String.Join(".", segments[0..^5]);
+    throw new NotImplementedException();
+  }
+
+  /// <summary>
+  /// The folder where the target bundle lives
+  /// </summary>
+  public string Folder { get; init; }
+
+  /// <summary>
+  /// The file prefix
+  /// </summary>
+  public string Prefix { get; init; }
+
+  /// <summary>
+  /// The tier of this backup. 0 for a full backup; 
+  /// values 1-9 indicate an incremental backup that references
+  /// another backup of <see cref="Tier"/> - 1.
+  /// </summary>
+  public int Tier { get; init; }
+
+  /// <summary>
+  /// The identifier for this bundle (derived from a timestamp in
+  /// yyyyMMdd-HHmm form)
+  /// </summary>
+  public string Id { get; init; }
+
+  /// <summary>
+  /// The identifier of the reference bundle, or null for tier 0 files.
+  /// </summary>
+  public string? RefId { get; init; }
+
+  /// <summary>
+  /// The short file name derived from the other fields
+  /// </summary>
+  public string BundleFileName { get; init; }
+
+  /// <summary>
+  /// The filename for the metadata
+  /// </summary>
+  public string MetaFileName { get; init; }
+}
--- gitbup/AppRun.fs
+++ gitbup/AppRun.fs
@@ -9,6 +9,47 @@ open LcGitLib2.RepoTools
 open ColorPrint
 open CommonTools
 
+type private RunOptions = {
+  RepoPath: string
+  Tier: int option
+}
+
+
 let runRun args =
-  failwith "NYI"
-  0
+  let rec parseMore o args =
+    match args with
+    | "-v" :: rest ->
+      verbose <- true
+      rest |> parseMore o
+    | "-h" :: _ ->
+      None
+    | [] ->
+      Some(o)
+    | "-C" :: path :: rest ->
+      rest |> parseMore {o with RepoPath = path}
+    | "-tier" :: "auto" :: rest ->
+      rest |> parseMore {o with Tier = None}
+    | "-tier" :: tier :: rest ->
+      let ok, n = tier |> Int32.TryParse
+      if ok |> not || n < 0 || n > 9 then
+        failwith $"Expecting the argument to '-tier' to be 'auto' or a single digit"
+      rest |> parseMore {o with Tier = Some(n)}
+    | "-full" :: rest ->
+      rest |> parseMore {o with Tier = Some(0)}
+    | x :: rest when x.Length = 3 && x.StartsWith("-t") && x[2]>='0' && x[2]<='9' ->
+      let n = int(x[2]-'0');
+      rest |> parseMore {o with Tier = Some(n)}
+    | x :: _ ->
+      failwith $"Unrecognized argument '{x}'"
+  let oo = args |> parseMore {
+    RepoPath = null
+    Tier = None
+  }
+  match oo with
+  | Some(o) ->
+    cp $"DBG: tier = %O{o.Tier}"
+    failwith "NYI"
+    0
+  | None ->
+    Usage.usage "run"
+    1
